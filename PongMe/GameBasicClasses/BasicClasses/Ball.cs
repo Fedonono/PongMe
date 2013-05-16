0828982deaaaf1eac8cf6b008df6bb0d2f528c5d
Janguy <site.a.janguy@gmail.com>
2013-05-16 23:12:28 +0200

    Test commit. 1ers fichiers très basiques.

---
+++ PongMe/GameBasicClasses/BasicClasses/Ball.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameBasicClasses.BasicClasses
+{
+    public class Ball
+    {
+        private float speed;
+        private readonly static float MAX_SPEED = 100;
+
+        public float Speed
+        {
+            get { return speed; }
+            set
+            {
+                if (Speed > 0 && Speed < MAX_SPEED)
+                {
+                    speed = value;
+                }
+            }
+        }
+
+        private float diameter;
+        private readonly static float MAX_DIAMETER = 50;
+
+        public float Diameter
+        {
+            get { return diameter; }
+            set
+            {
+                if (Diameter > 0 && Diameter < MAX_DIAMETER)
+                {
+                    diameter = value;
+                }
+            }
+        }
+
+        public Ball(float speed, float diameter)
+        {
+            this.Speed = speed;
+            this.Diameter = diameter;
+        }
+    }
+}
---
+++ PongMe/GameBasicClasses/MVC/Event.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameBasicClasses.BasicClasses
+{
+    public class Event
+    {
+        private Model sender;
+
+        public Event(Model s)
+        {
+            this.sender = s;
+        }
+
+        public Model getSender()
+        {
+            return sender;
+        }
+    }
+}
