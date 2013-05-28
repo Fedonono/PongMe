eb4dc438e0dd1348871016b17a8a2f86a47565cf
maxpv <maxence.prevost@gmail.com>
2013-05-28 13:44:42 +0200

    Ajout de la classe de options pour joueur

---
+++ PongMe/GameBasicClasses/Options/GamerOptions.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GameBasicClasses.Options
+{
+    public class GamerOptions
+    {
+        private Keys up;
+        private Keys down;
+
+        private static Keys stop;
+        private static Keys pause;
+
+        private static LinkedList<Keys> taken = new LinkedList<Keys>();
+
+        public GamerOptions(Keys up, Keys down)
+        {
+            this.Up = up;
+            this.Down = down;
+            this.Stop = Keys.Escape;
+            this.Pause = Keys.P;
+        }
+
+        public Keys Up
+        {
+            set
+            {
+                if (taken.Find(value) == null)
+                {
+                    this.up = value;
+                    taken.AddLast(value);
+                }
+            }
+
+            get;
+        }
+
+        public Keys Down
+        {
+            set
+            {
+                if (taken.Find(value) == null)
+                {
+                    this.down = value;
+                    taken.AddLast(value);
+                }
+            }
+
+            get;
+        }
+
+        public Keys Stop { get;
+            set
+            {
+                if (taken.Find(value) == null)
+                {
+                    this.Pause = value;
+                    taken.AddLast(value);
+                }
+            }
+        }
+
+        public Keys Pause
+        {
+            get;
+            set
+            {
+                if (taken.Find(value) == null)
+                {
+                    this.Pause = value;
+                    taken.AddLast(value);
+                }
+            }
+        }
+
+        public override String ToString()
+        {
+            return String.Format("{0} to go up - {1} to go down - {2} to stop the game - {3} to pause the game", 
+                this.Up, this.Down, this.Stop, this.Pause);
+        }
+    }
+}
