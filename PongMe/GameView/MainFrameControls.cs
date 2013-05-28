5d606ffd47fe8d609b99c96c2d21b38b79b36b5f
Fedonono <arnaud.babol@gmail.com>
2013-05-28 15:58:08 +0200

    ça va mieux avec les ressources...

---
+++ PongMe/GameView/MainFrameControls.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using GameBasicClasses.BasicClasses;
+using GameBasicClasses.Gamer;
+
+namespace GameView
+{
+    public partial class MainForm : Form
+    {
+        private void MainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (keysPressed.Contains(e.KeyCode))
+            {
+                keysPressed.Remove(e.KeyCode);
+            }
+            keysPressed.Add(e.KeyCode);
+        }
+
+        private void MainForm_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (keysPressed.Contains(e.KeyCode))
+            {
+                keysPressed.Remove(e.KeyCode);
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            List<Ball> listeBall = this.currentGame.GameModel.ListeBall;
+            foreach (Ball ball in listeBall)
+            {
+                ball.ClientSize = this.gameBoard.Size;
+                this.gameBoard.Controls.Add(ball.Box);
+            }
+            List<Gamer> listeGamer = this.currentGame.GameModel.ListeGamer;
+            foreach (Gamer gamer in listeGamer)
+            {
+                gamer.Paddle.ClientSize = this.gameBoard.Size;
+                this.gameBoard.Controls.Add(gamer.Paddle.Box);
+            }
+            this.leftPointsLabel.Text = this.currentGame.getPoints(true).ToString();
+            this.rightPointsLabel.Text = this.currentGame.getPoints(false).ToString();
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            foreach (Ball ball in this.currentGame.GameModel.ListeBall)
+            {
+                ball.nextPosition();
+                if (ball.isOutRight && ball.isMoving)
+                {
+                    ball.isMoving = false;
+                    this.currentGame.addPoint(false);
+                }
+                else if (ball.isOutLeft && ball.isMoving)
+                {
+                    ball.isMoving = false;
+                    this.currentGame.addPoint(true);
+                }
+            }
+
+            if (keysPressed.Count > 0)
+            {
+                foreach (Keys key in keysPressed)
+                {
+                    this.currentGame.keyEvent(key);
+                }
+            }
+            else
+            {
+                this.currentGame.keyEvent(Keys.A);//AI
+            }
+
+            if (this.currentGame.isGameOver())
+            {
+                this.currentGame.StopGame();
+            }
+            this.gameBoard.Refresh();
+        }
+
+        private void animationTimer_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bonusTimer_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void brickTimer_Tick(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
